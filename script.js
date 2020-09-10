397272b8b356421bc579e4f1c878dbd423cb8d9b
Nikita <mispister@yandex.ru>
2020-09-10 18:13:10 +0500

    Тестовый коммит

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+//  console.log("" + false);
+// console.log("https://vk.com/catalog/" + 5);
+
+// console.log(typeof(Number('5')));
+// console.log(+'5');
+// console.log(typeof(parseInt('15px', 10)));
+
+// let ans = +prompt("Hello?", '');
+//  0, '', null, undefined, NaN
+
+
+// let switcher = null;
+// if (switcher) {
+//     console.log("Working...");
+// }
+
+// switcher = 1;
+// if (switcher) {
+//     console.log("Not working...");
+// }
+
+// //2)
+// console.log(typeof(Boolean('5')));
+// //3)
+// console.log(typeof(!!'5'));
+
+// let answer = confirm("You gay?");
+// console.log(answer);
+
+// let answer = prompt("U gay?", "Sure");
+// console.log(typeof(null));
+
+// let number = 10,
+//     number2 = 20,
+//     number3 = 30;
+
+// number++;
+// number2--;
+
+
+// console.log(number);
+// console.log(number2);
+
+// console.log(5%2);
+// console.log(5==2);
+// console.log('2'=== 2);
+
+let isChecked  = true;
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(!isChecked);
